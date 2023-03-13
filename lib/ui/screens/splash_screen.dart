869bf634ac032acd01c3637baa64413ccac66267
gm_kafi <kaficsebaiust02@gmail.com>
2023-03-13 11:36:22 +0600

    init commit

---
+++ lib/ui/screens/splash_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double ScreenHeight = MediaQuery.of(context).size.height;
+    double ScreenWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          SvgPicture.asset(
+            'assets/image/background.svg',
+            fit: BoxFit.cover,
+            width: ScreenWidth,
+            height: ScreenHeight,
+          ),
+
+          Center(child: SvgPicture.asset('assets/image/logo.svg',))
+
+        ],
+      ),
+    );
+  }
+}

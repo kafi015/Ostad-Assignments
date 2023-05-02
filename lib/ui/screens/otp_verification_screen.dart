e2b27d0a10cadfa74a2209fc08349bca82e54a75
gm_kafi <kaficsebaiust02@gmail.com>
2023-05-02 19:30:17 +0600

    otp pin varification ui done

--- lib/ui/screens/otp_verification_screen.dart
+++ lib/ui/screens/otp_verification_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pin_code_fields/pin_code_fields.dart';
 import 'package:task_manager/ui/screens/log_in_screen.dart';
 import 'package:task_manager/ui/utils/text_styles.dart';
 import 'package:task_manager/ui/widgets/app_sign_in_out_text.dart';
@@ -37,13 +38,47 @@ class _OTPVarificationScreenState extends State<OTPVarificationScreen> {
               const SizedBox(
                 height: 24,
               ),
-              // AppTextField(
-              //   hintText: "Email",
-              //   controller: TextEditingController(),
-              // ),
+              PinCodeTextField(
+                length: 6,
+                obscureText: false,
+                animationType: AnimationType.fade,
+                pinTheme: PinTheme(
+                  shape: PinCodeFieldShape.box,
+                  borderRadius: BorderRadius.circular(8),
+                  fieldHeight: 50,
+                  fieldWidth: 40,
+                  activeFillColor: Colors.white,
+                  selectedFillColor: Colors.white,
+                  inactiveFillColor: Colors.white,
+                  activeColor: Colors.green,
+                  inactiveColor: Colors.red,
+                  selectedColor: Colors.green,
+                ),
+                animationDuration: Duration(milliseconds: 300),
+                backgroundColor: Colors.transparent,
+                enableActiveFill: true,
+
+
+                onCompleted: (v) {
+                  print("Completed");
+                },
+                onChanged: (value) {
+                  print(value);
+                  setState(() {
+
+                  });
+                },
+                beforeTextPaste: (text) {
+                  print("Allowing to paste $text");
+                  //if you return true then it will show the paste confirmation dialog. Otherwise if false, then nothing will happen.
+                  //but you can show anything you want here, like your pop up saying wrong paste format or etc
+                  return true;
+                }, appContext: context,
+              ),
               const SizedBox(
                 height: 24,
               ),
+
               AppElevatedButton(
                   child: const Text("Verify"),
                   onTap: () {
